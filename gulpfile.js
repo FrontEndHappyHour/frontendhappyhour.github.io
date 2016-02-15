e5d26741d476a0267941795896e2c22ea817e43b
ryanburgess <ryanb@netflix.com>
2016-02-15 14:40:44 -0800

    add ghooks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp        = require('gulp'),
-  sass          = require('gulp-sass'),
-  minifyCSS     = require('gulp-minify-css'),
-  rename        = require('gulp-rename'),
-  path          = require('path');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var path = require('path');
+var jshintStyle = require('jshint-stylish');
+var jsonlint = require('gulp-jsonlint');
 
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
@@ -11,8 +13,16 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./public/css/'));
 });
 
+// JSON Lint
+gulp.task('jsonlint', function() {
+  return gulp.src(['./content/*.json', './*.json'])
+    .pipe(jsonlint())
+    .pipe(jsonlint.reporter(jshintStyle));
+});
+
 gulp.task('watch', function() {
   gulp.watch('sass/**/*.scss', ['sass']);
 });
 
+gulp.task('test', ['jsonlint']);
 gulp.task('default', ['watch']);
