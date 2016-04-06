a81dc11f07a62dfa1c06ec59807965c5b9a7354e
ryanburgess <ryanb@netflix.com>
2016-04-05 20:41:58 -0700

    clean up clear function

--- clear.js
+++ clear.js
@@ -1,18 +1,19 @@
-var fs = require('fs');
-var cheerio = require('cheerio');
-var doc = '<!DOCTYPE html>\n<html>';
-var homePage = 'index.html';
+'use strict';
+const fs = require('fs');
+const cheerio = require('cheerio');
+const doc = '<!DOCTYPE html>\n<html>';
+const homePage = 'index.html';
 
 // read homepage
 fs.readFile('./index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  $ = cheerio.load(data);
+  const $ = cheerio.load(data);
   $('.episodes ol').empty();
 
-  var output = doc + $('html').html() + '</html>';
-  fs.writeFile(homePage, output, function(err) {
+  let output = doc + $('html').html() + '</html>';
+  fs.writeFile(homePage, output, function (err) {
     if(err) {
       console.log(err);
     } else {
