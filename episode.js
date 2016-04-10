2c920c800d7d7f0ae955ba5528970d87de35e8c2
ryanburgess <ryanb@netflix.com>
2016-04-10 14:01:08 -0700

    create a build script to add placeholder info for new episode

---
+++ episode.js
@@ -0,0 +1,51 @@
+'use strict';
+const fs            = require('fs');
+const episodes      = require('./content/episodes.json');
+const panelists     = require('./content/panelists.json');
+const jsonfile      = require('jsonfile');
+const epNum         = parseInt(episodes[0].episode) + 1;
+const names         = [];
+const picks         = [];
+const links         = [];
+const guests        = [];
+const temp          = [];
+
+// add padding to episode number
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+// create panelist list for new episode
+Object.keys(panelists).forEach(function(key) {
+  let name = panelists[key].name;
+  names.push(name);
+  picks.push({'title:': '', 'url': '', 'from': name });
+});
+
+// build up place holder object for new episode
+const object = {
+  "episode": pad(epNum, 3),
+  "title": "",
+  "description": "",
+  "id": "",
+  "published": "",
+  "panel": names,
+  "guests": guests,
+  "links": links,
+  "picks": picks
+};
+
+// add new object and combine with the existing episodes
+temp.push(object);
+temp.push(episodes[0]);
+
+// write new file
+jsonfile.writeFile('./content/episodes.json', temp, {spaces: 2}, function(err) {
+  if(err) {
+    console.log(err);
+  }else{
+    console.log(`Episodes updated with episode ${epNum}`);
+  }
+});
