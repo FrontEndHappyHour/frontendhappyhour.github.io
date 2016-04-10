3fd72d29cbb72b47de243906b2925781bfa649b3
ryanburgess <ryanb@netflix.com>
2016-04-10 14:54:01 -0700

    add eslint and clean up files

--- build.js
+++ build.js
@@ -1,33 +1,29 @@
-const fs            = require('fs');
-const cheerio       = require('cheerio');
-const mkdirp        = require('mkdirp');
-const path          = require('path');
-const _             = require('lodash');
-const episodes      = require('./content/episodes.json');
-const panelists     = require('./content/panelists.json');
-const doc           = '<!DOCTYPE html>\n<html>';
-const homePage      = 'index.html';
-const currentTitles = [];
-var output;
+'use strict';
+const fs = require('fs');
+const cheerio = require('cheerio');
+const mkdirp = require('mkdirp');
+const _ = require('lodash');
+const episodes = require('./content/episodes.json');
+const panelists = require('./content/panelists.json');
+const doc = '<!DOCTYPE html>\n<html>';
+const homePage = 'index.html';
+let output;
 
 // read homepage
-const page = fs.readFileSync('./index.html', 'utf8' );
-
-// read episode
-const epPage = fs.readFileSync('./episode.html', 'utf8' );
+const page = fs.readFileSync('./index.html', 'utf8');
 
 // contains prototype
-Array.prototype.contains = function ( needle ) {
-   for (i in this) {
-       if (this[i] == needle) return true;
+Array.prototype.contains = function (needle) {
+   for (let i in this) {
+      if (this[i] === needle) {
+        return true;
+      }
    }
    return false;
-}
-
-episodeContent();
+};
 
 // get episode json content
-function episodeContent(){
+function episodeContent() {
 
     const $ = cheerio.load(page);
     const ep = cheerio.load(page);
@@ -35,18 +31,17 @@ function episodeContent(){
     //just redo the episodes every time
     $('.episodes ol li').remove();
 
-    var panel;
-
-    for(var i = episodes.length - 1; i >= 0; i--) {
-      var epTitle = episodes[i].title;
+    let panel;
+    for(let i = episodes.length - 1; i >= 0; i--) {
+      const epTitle = episodes[i].title;
       panel = episodes[i].panel;
-        var epDate = episodes[i].published;
-        var epDesc = episodes[i].description;
-        var link = epTitle.replace(/ /g, '-').toLowerCase();
-        var id = episodes[i].id;
-        var picks = episodes[i].picks;
-        var links = episodes[i].links;
-        var guests = episodes[i].guests;
+      const epDate = episodes[i].published;
+      const epDesc = episodes[i].description;
+      const link = epTitle.replace(/ /g, '-').toLowerCase();
+      const id = episodes[i].id;
+      const picks = episodes[i].picks;
+      const links = episodes[i].links;
+      const guests = episodes[i].guests;
 
         // create list of episodes on the homepage
         $('.episodes ol').prepend(
@@ -64,10 +59,10 @@ function episodeContent(){
         ep('meta[name=description]').attr('content', epDesc);
 
         //update CSS to episode css
-        ep('link[href="public/css/style.css"]').attr('href','../../public/css/episode.css');
+        ep('link[href="public/css/style.css"]').attr('href', '../../public/css/episode.css');
 
         ep('img').each(function() {
-          if(ep(this).attr('src').indexOf('../../') === -1){
+          if(ep(this).attr('src').indexOf('../../') === -1) {
             ep(this).attr('src', '../../' + ep(this).attr('src'));
           }
         });
@@ -77,17 +72,19 @@ function episodeContent(){
           `<h2>${epTitle}</h2>` +
           `<time>Published ${epDate}</time>` +
           `<div class="audio">` +
-          `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${id}&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>` +
+          `<iframe width="100%" height="166" scrolling="no" frameborder="no"
+           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/
+          ${id}&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>` +
           `</div>` +
           `<p>${epDesc}</p>`
         );
 
-        if(guests.length !== 0){
-          var guestList = '';
-          for(var b = 0; b < guests.length; b++) {
+        if(guests.length !== 0) {
+          let guestList = '';
+          for(let b = 0; b < guests.length; b++) {
             guestList += '<li>' +
             '<a href="https://twitter.com/' + guests[b].twitter + '">' + guests[b].name + '</a>' +
-            '</li>'
+            '</li>';
           }
           ep('.episodes').append(
             '<div class="guests picks">' +
@@ -98,11 +95,11 @@ function episodeContent(){
           );
         }
 
-        if(links.length !== 0){
-          var linkContent = '';
-          for(var b = 0; b < links.length; b++) {
-            linkContent += '<a href="' + links[b].url + '">' + links[b].title + '</a>';
-            if(b !== links.length -1){
+        if(links.length !== 0) {
+          let linkContent = '';
+          for(let c = 0; c < links.length; c++) {
+            linkContent += '<a href="' + links[c].url + '">' + links[c].title + '</a>';
+            if(c !== links.length -1) {
               linkContent += ', ';
             }
           }
@@ -112,16 +109,16 @@ function episodeContent(){
             '<p>' + linkContent +
             '</p>' +
             '</div>'
-          );          
+          );
         }
 
         // if picks add picks
-        if(picks.length !== 0){
-          var picksContent = '';
-          for(var a = 0; a < picks.length; a++) {
+        if(picks.length !== 0) {
+          let picksContent = '';
+          for(let a = 0; a < picks.length; a++) {
             picksContent += '<li>' +
               '<a href="' + picks[a].url + '">' + picks[a].title + '</a>' + ' - ' + picks[a].from +
-              '</li>'
+              '</li>';
           }
 
           ep('.episodes').append(
@@ -138,17 +135,15 @@ function episodeContent(){
           );
         }
 
-        for(var x = 0; x < panelists.length; x++) {
-            if(panel.contains(panelists[x].name)){
-              var name = panelists[x].name;
-              var pic = panelists[x].profile_pic;
-              var picMarkup = '';
-              var twitter = panelists[x].twitter;
-
-              if( _.startsWith( pic, 'http') || _.startsWith( pic, '/' )){
+        for(let x = 0; x < panelists.length; x++) {
+            if(panel.contains(panelists[x].name)) {
+              const name = panelists[x].name;
+              const pic = panelists[x].profile_pic;
+              const twitter = panelists[x].twitter;
+              let pickMarkup;
+              if(_.startsWith(pic, 'http') || _.startsWith(pic, '/')) {
                 pickMarkup = `<img src="../../public${pic}" alt="${name} profile picture" />`;
-              }
-              else{
+              }else {
                 pickMarkup = `<img src="https://avatars0.githubusercontent.com/u/${pic}?v=3&s=150" alt="${name} profile picture">`;
               }
 
@@ -161,9 +156,9 @@ function episodeContent(){
             }
           }
 
-          var newEpOutput = doc + ep('html').html() + '</html>';
+          const newEpOutput = doc + ep('html').html() + '</html>';
 
-          mkdirp.sync( `./episodes/${link}` );
+          mkdirp.sync(`./episodes/${link}`);
 
           fs.writeFileSync(`./episodes/${link}/index.html`, newEpOutput);
     }
@@ -178,3 +173,5 @@ function episodeContent(){
       }
     });
 }
+
+episodeContent();
