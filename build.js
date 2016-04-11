a9310b7442f282f0cf929ad40847a0f868abdbea
ryanburgess <ryanb@netflix.com>
2016-04-10 19:59:26 -0700

    clean up formatting

--- build.js
+++ build.js
@@ -43,124 +43,124 @@ function episodeContent() {
       const links = episodes[i].links;
       const guests = episodes[i].guests;
 
-        // create list of episodes on the homepage
-        $('.episodes ol').prepend(
-          `<li><a href="episodes/${link}/"><h3>${epTitle}</h3>` +
-          `<time>${epDate}</time>` +
-          `<p>${epDesc}</p>` +
-          `</a></li>`
+      // create list of episodes on the homepage
+      $('.episodes ol').prepend(
+        `<li><a href="episodes/${link}/"><h3>${epTitle}</h3>` +
+        `<time>${epDate}</time>` +
+        `<p>${epDesc}</p>` +
+        `</a></li>`
+      );
+
+      // create html page for new episode
+      // create new Title
+      ep('title').html(epTitle + ' - Front End Happy Hour');
+
+      // create new page description
+      ep('meta[name=description]').attr('content', epDesc);
+
+      //update CSS to episode css
+      ep('link[href="public/css/style.css"]').attr('href', '../../public/css/episode.css');
+
+      ep('img').each(function() {
+        if(ep(this).attr('src').indexOf('../../') === -1) {
+          ep(this).attr('src', '../../' + ep(this).attr('src'));
+        }
+      });
+
+      // add episode content info
+      ep('.episodes').html(
+        `<h2>${epTitle}</h2>` +
+        `<time>Published ${epDate}</time>` +
+        `<div class="audio">` +
+        `<iframe width="100%" height="166" scrolling="no" frameborder="no"
+         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/
+        ${id}&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>` +
+        `</div>` +
+        `<p>${epDesc}</p>`
+      );
+
+      if(guests.length !== 0) {
+        let guestList = '';
+        for(let b = 0; b < guests.length; b++) {
+          guestList += '<li>' +
+          '<a href="https://twitter.com/' + guests[b].twitter + '">' + guests[b].name + '</a>' +
+          '</li>';
+        }
+        ep('.episodes').append(
+          '<div class="guests picks">' +
+          '<h3>Guests</h3>' +
+          '<ul>' + guestList +
+          '</ul>' +
+          '</div>'
         );
+      }
 
-        // create html page for new episode
-        // create new Title
-        ep('title').html(epTitle + ' - Front End Happy Hour');
-
-        // create new page description
-        ep('meta[name=description]').attr('content', epDesc);
-
-        //update CSS to episode css
-        ep('link[href="public/css/style.css"]').attr('href', '../../public/css/episode.css');
-
-        ep('img').each(function() {
-          if(ep(this).attr('src').indexOf('../../') === -1) {
-            ep(this).attr('src', '../../' + ep(this).attr('src'));
+      if(links.length !== 0) {
+        let linkContent = '';
+        for(let c = 0; c < links.length; c++) {
+          linkContent += '<a href="' + links[c].url + '">' + links[c].title + '</a>';
+          if(c !== links.length -1) {
+            linkContent += ', ';
           }
-        });
-
-        // add episode content info
-        ep('.episodes').html(
-          `<h2>${epTitle}</h2>` +
-          `<time>Published ${epDate}</time>` +
-          `<div class="audio">` +
-          `<iframe width="100%" height="166" scrolling="no" frameborder="no"
-           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/
-          ${id}&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>` +
-          `</div>` +
-          `<p>${epDesc}</p>`
+        }
+        ep('.episodes').append(
+          '<div class="links picks">' +
+          '<h3>Items mentioned in the episode</h3>' +
+          '<p>' + linkContent +
+          '</p>' +
+          '</div>'
         );
+      }
 
-        if(guests.length !== 0) {
-          let guestList = '';
-          for(let b = 0; b < guests.length; b++) {
-            guestList += '<li>' +
-            '<a href="https://twitter.com/' + guests[b].twitter + '">' + guests[b].name + '</a>' +
+      // if picks add picks
+      if(picks.length !== 0) {
+        let picksContent = '';
+        for(let a = 0; a < picks.length; a++) {
+          picksContent += '<li>' +
+            '<a href="' + picks[a].url + '">' + picks[a].title + '</a>' + ' - ' + picks[a].from +
             '</li>';
-          }
-          ep('.episodes').append(
-            '<div class="guests picks">' +
-            '<h3>Guests</h3>' +
-            '<ul>' + guestList +
-            '</ul>' +
-            '</div>'
-          );
         }
 
-        if(links.length !== 0) {
-          let linkContent = '';
-          for(let c = 0; c < links.length; c++) {
-            linkContent += '<a href="' + links[c].url + '">' + links[c].title + '</a>';
-            if(c !== links.length -1) {
-              linkContent += ', ';
-            }
+        ep('.episodes').append(
+          '<div class="picks">' +
+          '<h3>Picks</h3>' +
+          '<ul>' + picksContent +
+          '</ul>' +
+          '</div>' +
+          '<div class="panel">' +
+          '<h3>Panel</h3>' +
+          '<ul>' +
+          '</ul>' +
+          '</div>'
+        );
+      }
+
+      for(let x = 0; x < panelists.length; x++) {
+        if(panel.contains(panelists[x].name)) {
+          const name = panelists[x].name;
+          const pic = panelists[x].profile_pic;
+          const twitter = panelists[x].twitter;
+          let pickMarkup;
+          if(_.startsWith(pic, 'http') || _.startsWith(pic, '/')) {
+            pickMarkup = `<img src="../../public${pic}" alt="${name} profile picture" />`;
+          }else {
+            pickMarkup = `<img src="https://avatars0.githubusercontent.com/u/${pic}?v=3&s=150" alt="${name} profile picture">`;
           }
-          ep('.episodes').append(
-            '<div class="links picks">' +
-            '<h3>Items mentioned in the episode</h3>' +
-            '<p>' + linkContent +
-            '</p>' +
-            '</div>'
+
+          ep('.panel ul').append(
+            `<li>${pickMarkup}` +
+            `<span class="name">${name}</span>` +
+            `<a href="https://twitter.com/${twitter}" class="twitter">@${twitter}</a>` +
+            `</li>`
           );
         }
+      }
 
-        // if picks add picks
-        if(picks.length !== 0) {
-          let picksContent = '';
-          for(let a = 0; a < picks.length; a++) {
-            picksContent += '<li>' +
-              '<a href="' + picks[a].url + '">' + picks[a].title + '</a>' + ' - ' + picks[a].from +
-              '</li>';
-          }
+      const newEpOutput = doc + ep('html').html() + '</html>';
 
-          ep('.episodes').append(
-            '<div class="picks">' +
-            '<h3>Picks</h3>' +
-            '<ul>' + picksContent +
-            '</ul>' +
-            '</div>' +
-            '<div class="panel">' +
-            '<h3>Panel</h3>' +
-            '<ul>' +
-            '</ul>' +
-            '</div>'
-          );
-        }
+      mkdirp.sync(`./episodes/${link}`);
 
-        for(let x = 0; x < panelists.length; x++) {
-            if(panel.contains(panelists[x].name)) {
-              const name = panelists[x].name;
-              const pic = panelists[x].profile_pic;
-              const twitter = panelists[x].twitter;
-              let pickMarkup;
-              if(_.startsWith(pic, 'http') || _.startsWith(pic, '/')) {
-                pickMarkup = `<img src="../../public${pic}" alt="${name} profile picture" />`;
-              }else {
-                pickMarkup = `<img src="https://avatars0.githubusercontent.com/u/${pic}?v=3&s=150" alt="${name} profile picture">`;
-              }
-
-              ep('.panel ul').append(
-                `<li>${pickMarkup}` +
-                `<span class="name">${name}</span>` +
-                `<a href="https://twitter.com/${twitter}" class="twitter">@${twitter}</a>` +
-                `</li>`
-              );
-            }
-          }
-
-          const newEpOutput = doc + ep('html').html() + '</html>';
-
-          mkdirp.sync(`./episodes/${link}`);
-
-          fs.writeFileSync(`./episodes/${link}/index.html`, newEpOutput);
+      fs.writeFileSync(`./episodes/${link}/index.html`, newEpOutput);
     }
 
     output = doc + $('html').html() + '</html>';
