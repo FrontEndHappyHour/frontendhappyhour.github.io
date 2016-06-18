ed6a74ed4c56ab875bdd732fff1aeed3912cf2df
ryanburgess <ryanb@netflix.com>
2016-06-17 17:40:57 -0700

    update form

--- templates/mailing-list.js
+++ templates/mailing-list.js
@@ -2,7 +2,7 @@
 module.exports = function mailing(title) {
   'use strict';
 
-  return `<form action="//frontendhappyhour.us13.list-manage.com/subscribe/post?u=27e5479493204609f037a4f6a&amp;id=95d9b45a38" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+  return `<form action="//frontendhappyhour.us13.list-manage.com/subscribe/post?u=27e5479493204609f037a4f6a&amp;id=039ab8ada1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
           <label for="mce-EMAIL">Email address:</label>
           <input type="email" value="" name="EMAIL" id="mce-EMAIL">
           <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_27e5479493204609f037a4f6a_95d9b45a38" tabindex="-1" value=""></div>
