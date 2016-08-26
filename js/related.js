34f4956e4fd37ee596d2003bd0d49e43a647b439
ryanburgess <ryanb@netflix.com>
2016-08-26 12:29:28 -0600

    update build

--- js/related.js
+++ js/related.js
@@ -1,4 +1,12 @@
-fetch('content/episodes.json')
-.then(function(response) {
-  console.log(response.blob());
+const episodeTitle = document.getElementsByTagName('h2');
+const episodes = 'https://raw.githubusercontent.com/FrontEndHappyHour/frontendhappyhour.github.io/master/content/episodes.json';
+
+fetch(episodes).then(function(response) {
+  const contentType = response.headers.get('content-type');
+  return response.json().then(function(json) {
+    for (let episodes of json) {
+      const title = episodes.title;
+      const category = episodes.category;
+    }
+  });
 });
