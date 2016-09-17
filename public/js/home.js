c840055d94b4f4b0345eaf52ee15026278f0fd99
ryanburgess <ryanb@netflix.com>
2016-09-17 10:23:49 -0700

    update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -140,7 +140,7 @@ module.exports= [
         "url": "https://facebook.github.io/react/"
       },
       {
-        "title": "Anuglar",
+        "title": "Angular",
         "url": "https://angularjs.org/"
       },
       {
