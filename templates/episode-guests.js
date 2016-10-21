093d9b4150e4be917cdb715a64d659a0e1a601ca
ryanburgess <ryanb@netflix.com>
2016-10-21 08:17:50 -0700

    update guest template

--- templates/episode-guests.js
+++ templates/episode-guests.js
@@ -9,18 +9,18 @@ module.exports = function episodeGuests(guests) {
     // download profile pics if they don't already exist
     const twitterGuest = guests[i].twitter;
     if(twitterGuest.indexOf('http') === -1) {
-      // download Twitter photo
-      fs.stat(`./public/img/guests/${twitterGuest}.jpg`, function(err, stat) {
-        if(err !== null) {
-          twitterPic(twitterGuest, 'public/img/guests');
-        }
-      });
-
       let twitterImage = twitterGuest;
       // if twitter name starts with _ than remove _
       if (twitterImage.substring(0, 1) === '_') {
         twitterImage = twitterImage.substring(1);
       }
+
+      // download Twitter photo
+      fs.stat(`./public/img/guests/${twitterImage}.jpg`, function(err, stat) {
+        if(err !== null) {
+          twitterPic(twitterGuest, 'public/img/guests');
+        }
+      });
 
       guestList += `<li><img src="../../public/img/guests/${twitterImage}.jpg" alt="${guests[i].name} profile picture"><div><a href="https://twitter.com/${twitterGuest}">${guests[i].name}</a></div></li>`;
 
