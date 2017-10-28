dd9e7e1b81d9b533c22d36c51f3ff99838ac435b
Lachlan Heywood <lachlan@morrisonagency.com>
2017-10-28 13:26:15 -0400

    Add invalid URLs array to catch invalids easier

--- tests/url-test.js
+++ tests/url-test.js
@@ -3,17 +3,25 @@ const test = require('tape');
 const strings = require('../content/strings.json');
 const isURL = require('../lib/is-url');
 
+let invalidURLs = [];
 //test URLs in strings.json to make sure they are valid URLs
 test('Test URLs', function (t) {
   // loop through list of URLs
-  for (let i = 0; i < strings.urls.length; i++) {
-    const urlTest = isURL(strings.urls[0].twitter);
+  for (let i = 0; i < strings.urls[0].length; i++) {
+    const urlTest = isURL(strings.urls[0][i]);
 
     // test to make sure URL is returns true
-    t.equal(urlTest, true);
+    if (!t.equal(urlTest, true)) {
+			// Add Invalid URLs to array for easier debugging
+			invalidURLs.push(strings.urls[0][i]);
+		}
   }
-
-  t.comment('URLs are all valid');
+	if (invalidURLs.length > 0) {
+		t.comment('Invalid URLs:');
+		t.comment(invalidURLs.join('\n'));
+	} else {
+		t.comment('URLs are all valid');
+	}
 
   t.end();
 });
