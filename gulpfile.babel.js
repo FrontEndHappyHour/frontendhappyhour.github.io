3adde91508e2c525bc727f94bfef8850524148b0
Lachlan Heywood <lachlan@morrisonagency.com>
2018-10-16 00:33:06 -0400

    update to gulp 4.0.0 and remove insecure packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,84 +10,84 @@ const browserify = require('browserify');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const source = require('vinyl-source-stream');
+const merge = require('merge-stream');
+const log = require('fancy-log');
 
-gulp.task('sass', function () {
-  return gulp.src('sass/**/*.scss')
+gulp.task('sass', () => gulp.src('sass/**/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./public/css/'));
-});
+    .pipe(gulp.dest('./public/css/'))
+);
 
-gulp.task('svgmin', function () {
-    return gulp.src('public/img/**/*.svg')
-        .pipe(svgmin())
-        .pipe(gulp.dest('./public/img/'));
-});
+gulp.task('svgmin', () => gulp.src('public/img/**/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('./public/img/'))
+);
 
 // ESLint
-gulp.task('lint', function () {
-  return gulp.src(['**/*.js', '!node_modules/**', '!public/js/**/*.js'])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError());
-});
+gulp.task('lint', () => gulp.src(['**/*.js', '!node_modules/**', '!public/js/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
 
 // JSON Lint
-gulp.task('jsonlint', function() {
-  return gulp.src(['./content/*.json', './*.json'])
+gulp.task('jsonlint', () => gulp.src(['./content/*.json', './*.json'])
     .pipe(jsonlint())
-    .pipe(jsonlint.reporter(jshintStyle));
-});
+    .pipe(jsonlint.reporter(jshintStyle))
+);
 
 // nodeunit tests
-gulp.task('nodeunit', function () {
-  gulp.src('tests/**/*.js')
+gulp.task('nodeunit', () => gulp.src('tests/**/*.js')
     .pipe(nodeunit({
       reporter: 'junit',
       reporterOptions: {
         output: 'test-output'
       }
-    }));
-});
+    }))
+);
 
 // build javascript bundles
-gulp.task('javascript', function() {
- const jsxPath = './jsx/';
- const files = ['home', /*'panelist', 'episode'*/];
- const streams = files.map(function(fileName) {
-  const fullFile = jsxPath + fileName + '.jsx';
-  const bundler = browserify({
-    extensions: ['.js', '.jsx'],
-    transform: [['babelify', { presets: ['react', 'es2015'], plugins: ['transform-class-properties'] }]]
-  });
+gulp.task('javascript', done => {
+  const jsxPath = './jsx/';
+  const files = ['home'];
+  return merge(files.map(fileName => {
+    const fullFile = jsxPath + fileName + '.jsx';
+    const bundler = browserify({
+      extensions: ['.js', '.jsx'],
+      transform: [['babelify', { presets: ['react', 'es2015'], plugins: ['transform-class-properties'] }]]
+    });
 
-  bundler.add(fullFile);
+    bundler.on('error', err => { log.error(err.toString()); });
 
-  const stream = bundler.bundle();
-  stream.on('error', function (err) { console.error(err.toString()); });
+    log.info(`Compiling File '${fileName}.jsx'.`);
 
-  stream
-    .pipe(source(fullFile))
-    .pipe(rename(fileName + '.js'))
-    .pipe(gulp.dest('public/js/'));
-  console.log(`${fileName}.js created`);
- });
+    return bundler.bundle()
+      .pipe(source(fullFile))
+      .pipe(rename(fileName + '.js'))
+      .pipe(gulp.dest('public/js/'));
+  }));
 });
 
-gulp.task('compress', function() {
-  return gulp.src('./public/js/*.js')
+gulp.task('compress', () => gulp.src('./public/js/*.js')
     .pipe(uglify())
     .pipe(rename({
        extname: '.min.js'
      }))
-    .pipe(gulp.dest('./public/js/min/'));
-});
+    .pipe(gulp.dest('./public/js/min/'))
+);
 
-gulp.task('watch', function() {
-  gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task('scripts', gulp.parallel(['lint', 'javascript', 'compress']));
+
+gulp.task('watch', done => {
+  gulp.watch('sass/**/*.scss', gulp.series(['sass']));
   //gulp.watch(['**/*.js', '!node_modules/**'], ['lint', 'nodeunit']);
-  gulp.watch(['./jsx/**/*'], ['lint', 'javascript', 'compress']);
+  gulp.watch(['./jsx/**/*'], gulp.series(['scripts']));
+  done();
 });
 
-gulp.task('test', ['jsonlint', 'lint', 'nodeunit']);
-gulp.task('default', ['watch']);
+gulp.task('test', gulp.series(['jsonlint', 'lint', 'nodeunit']));
+
+gulp.task('build', gulp.parallel(['scripts', 'sass', 'svgmin']));
+
+gulp.task('default', gulp.series(['build', 'test', 'watch']));
