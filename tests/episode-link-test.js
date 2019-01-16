92b0e29a37e846dfc742f178a41abc91f5e34eb7
Augustus Yuan <ayuan@evernote.com>
2019-01-15 16:28:40 -0800

    Fix lint, header-test, and episode-link-tests

--- tests/episode-link-test.js
+++ tests/episode-link-test.js
@@ -3,7 +3,8 @@ const test = require('tape');
 const episodes = require('../content/episodes.json');
 const isURL = require('../lib/is-url');
 
-//test URLs in strings.json to make sure they are valid URLs
+// test URLs in strings.json to make sure they are valid URLs
+// URLs can be blank because sometimes guests do not pick something on the internet
 test('Test URLs', function (t) {
   const invalidPicks = [];
   const invalidLinks = [];
@@ -13,22 +14,22 @@ test('Test URLs', function (t) {
     const links = episodes[i].links;
 
     for (const pick of picks) {
-      const pickUrlTest = isURL(pick.url);
+      const pickUrlTest = isURL(pick.url) || pick.url === '';
       t.equal(pickUrlTest, true, `"${pick.url}" is valid`);
 
       // Add Invalid URLs to array for easier debugging
       if (!pickUrlTest) {
-        invalidPicks.push(pick.url);
+        invalidPicks.push(JSON.stringify(pick));
       }
     }
 
     for (const link of links) {
-      const linkUrlTest = isURL(link.url);
+      const linkUrlTest = isURL(link.url) || link.url === '';
       t.equal(linkUrlTest, true, `"${link.url}" is valid`);
 
       // Add Invalid URLs to array for easier debugging
       if (!linkUrlTest) {
-        invalidLinks.push(link.url);
+        invalidLinks.push(JSON.stringify(link));
       }
     }
   }
