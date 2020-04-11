15941bba2e99103c2061bd63f7f0be079e396e20
ryanburgess <burgessdryan@gmail.com>
2020-04-11 11:36:27 -0700

    update date for virtual happy hour

--- content/virtual-happy-hour.js
+++ content/virtual-happy-hour.js
@@ -10,7 +10,7 @@ module.exports = `
           
           <p>We hope to see you there! And remember to stay safe.</p>
           
-          <p>Thanks for joining us for the Front End Virtual Happy Hour! We’re excited to have you join :) We’ll be hosting an AMA (Ask me Anything) live session on <a href="https://twitch.tv/frontendhappyhour">twitch.tv/frontendhappyhour</a> on Thursday, April 18th at 8:00 PM PST.</p>
+          <p>Thanks for joining us for the Front End Virtual Happy Hour! We’re excited to have you join :) We’ll be hosting an AMA (Ask me Anything) live session on <a href="https://twitch.tv/frontendhappyhour">twitch.tv/frontendhappyhour</a> on Thursday, April 16th at 8:00 PM PST.</p>
 
           <p>If you’d like to ask questions, simply create an account on Twitch to participate in the chat! You may also tweet us <a href="https://twitter.com/frontendhh">@FrontendHH</p> if you have any questions you want to ask and we’ll try our best to get to them.</p>
           <h2 id="heading">What to expect</h2>
