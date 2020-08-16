209c2844cd0de6e812937be5637d590095e72ca8
ryanburgess <burgessdryan@gmail.com>
2020-08-16 16:07:45 -0700

    remove episodes from nav

--- templates/header.js
+++ templates/header.js
@@ -13,7 +13,6 @@ module.exports = function header(path) {
                 <ul class="nav">
                     <li><a href="/">Home</a></li>
                     <li><a href="/about/">About</a></li>
-                    <li><a href="/episodes/">Episodes</a></li>
                     <li><a href="/subscribe/">Subscribe</a></li>
                 </ul>
             </div>
