13a36694d13b29936f3d37f20d07da3655b73cad
ryanburgess <burgessdryan@gmail.com>
2022-01-13 15:49:17 -0800

    extend tweet picks script for other panelists to leverage with a panelist parameter

--- tweet-picks.js
+++ tweet-picks.js
@@ -1,9 +1,25 @@
+// How to leverage the script
+// run: node tweet-picks [pass first name or full name of panelist]
+// Default will return the latest episode
+// If you want to pass an additional parameter of episode number to get a specific episode picks
+// run: node tweet-picks [pass first name or full name of panelist] [episode number]
+
 'use strict';
 const episodes = require('./content/episodes.json');
 const panelInfo = require('./content/panelists.json');
 const createUrl = require('./lib/create-url');
 const args = process.argv.slice(2);
-const episodeNum = args[0];
+let personName = args[0];
+let episodeNum = args[1];
+
+// allow name parameter to accept lowercase but convert first and last name uppercase first letter
+personName = personName.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+
+// remove any letters from the episode number parameter
+episodeNum = episodeNum.replace(/\D/g,'');
 
 function createDetails(num) {
   let epNum;
@@ -17,20 +33,21 @@ function createDetails(num) {
   const link = 'https://frontendhappyhour.com/episodes/' + createUrl(epTitle);
 
   // Start of tweet
-  desc += `Here are my picks from the latest episode of the Front End Happy Hour podcast ${link}\n\n`
+  desc += `Here are my picks from the latest episode of the Front End Happy Hour podcast @FrontEndHH ${link}\n\n`
 
   // get pick details
   const picks = episodes[num].picks;
   if(picks.length !== 0) {
     for(let pick in picks) {
         const pickOwner = picks[pick].from;
-        if (pickOwner == 'Ryan Burgess'){
+        // only out put for the panelist from the parameter
+        if (pickOwner.includes(personName)){
             desc += picks[pick].title + ' ' + picks[pick].url + `\n`;
         }
     }
   }
 
-  // display final details
+  // display final content for the tweet
   console.log(desc);
 }
 
