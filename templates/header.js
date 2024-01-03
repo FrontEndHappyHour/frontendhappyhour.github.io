fd74d071b103ecb9b193139a1e24ab4193864dfb
Ryan Burgess <burgessdryan@gmail.com>
2024-01-03 12:32:30 -0800

    update nav

--- templates/header.js
+++ templates/header.js
@@ -14,7 +14,6 @@ module.exports = function header(path) {
                     <li><a href="/">Home</a></li>
                     <li><a href="/about/">About</a></li>
                     <li><a href="/subscribe/">Subscribe</a></li>
-                    <li><a href="/advertise/">Advertise</a></li>
                 </ul>
             </div>
         </header>`;
